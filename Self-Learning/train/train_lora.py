46d45bcb98e9678882bfa31c25b7163d48232c1f
翟增鑫 <2393893085@qq.com>
2025-12-04 17:42:04 +0800

    美化微调代码

--- Self-Learning/train/train_lora.py
+++ Self-Learning/train/train_lora.py
@@ -1,48 +1,27 @@
-# Usage: deepspeed train_lora.py --deepspeed <$PATH_TO_DEEPSPEED_CONFIG>
-
-# Adapted from tatsu-lab@stanford_alpaca. Below is the original copyright:
-#    Copyright 2023 Rohan Taori, Ishaan Gulrajani, Tianyi Zhang, Yann Dubois, Xuechen Li
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-
-import json
 from dataclasses import dataclass, field
 import logging
 import pathlib
 import typing
 import os
 from datasets import load_dataset
-
 from deepspeed import zero
 from deepspeed.runtime.zero.partition_parameters import ZeroParamStatus
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 import transformers
 from transformers import Trainer, BitsAndBytesConfig, deepspeed
 import torch
-
 from fastchat.train.train import (
     DataArguments,
     ModelArguments,
-    make_supervised_data_module,
     rank0_print
 )
-
 from fastchat.train.llama2_flash_attn_monkey_patch import (
     replace_llama_attn_with_flash_attn,
 )
 
 FULL_PROMPT_TEMPLATE = "{input}\n### Response:\n{output}"
 NO_OUTPUT_PROMPT_TEMPLATE = "{input}\n### Response:\n"
+
 
 @dataclass
 class TrainingArguments(transformers.TrainingArguments):
@@ -155,8 +134,6 @@ def load_and_process_dataset(tokenizer, data_args):
     train_data = train_data["train"].shuffle().map(generate_and_tokenize_prompt)
     return train_data, eval_data
         
-    
-
 
 def train():
     parser = transformers.HfArgumentParser(
